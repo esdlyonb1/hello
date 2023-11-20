fc80e9cf6db72984e3d050f9bec1fb7d36006f0b
esdb1 <pierre.lacognata@mail-esd.com>
2023-11-20 15:37:37 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$prenom = "je sais pas comment tu t'appelles";
+// $_GET
+
+?>
+
+<h2>Hello <?= $prenom ?> </h2>
+
+<form method="get" action="">
+
+    <input type="text" name="prenom" id="">
+    <button type="submit">OK</button>
+
+</form>
+
+</body>
+</html>
